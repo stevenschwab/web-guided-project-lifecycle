f641c9cf9861c51fbc93e3e3d1adda993d5c001a
Steve Schwab <schwabs@umich.edu>
2024-02-05 13:11:15 -0800

    add lifecycle methods

--- doggos/src/App.js
+++ doggos/src/App.js
@@ -1,2 +1,40 @@
 import React from 'react';
 import axios from 'axios';
+
+const fetchDogs = (breed) => {
+    return axios.get(`https://dog.ceo/api/breed/${breed}/images`).then(res => res).catch(err => console.error('error'));
+}
+
+export default class App extends React.Component {
+    constructor() {
+        console.log('constructor ran');
+        super();
+        this.state = {
+            doggos: [],
+            breed: 'husky'
+        }
+    }
+
+    componentDidMount() {
+        console.log('component did mount')
+        fetchDogs(this.state.breed).then(res => {
+            this.setState({ doggos: res.data.message });
+        })
+    }
+
+    componentDidUpdate(prevProps,prevState) {
+        console.log('component did update')
+        console.log(prevState)
+    }
+
+    render() {
+        console.log('render function ran');
+        return (
+            <>
+                <h1>My App</h1>
+                {this.state.doggos.map((dog, index) => (
+                    <img width="200" src={dog} key={index} alt={dog} />))}
+            </>
+        )
+    }
+}
